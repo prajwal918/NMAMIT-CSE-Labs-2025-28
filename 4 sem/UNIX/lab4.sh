d41ba2249fd4fbc484ba4954bf20552034530245
prajwal918 <prajwal918@users.noreply.github.com>
2026-03-16 20:29:56 +0530

    Update lab4.sh to include both interactive and command line parts

--- 4 sem/UNIX/lab4.sh
+++ 4 sem/UNIX/lab4.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 # Experiment 4: Divisibility Check
+
 # a) Read from keyboard (interactive):
+echo "a) Read from keyboard (interactive):"
 read -p "Enter the value of n and m: " n m
 y=`expr $n % $m`
-
 if [ $y -eq 0 ]
 then
     echo "$n is divisible by $m"
 else
     echo "$n is not divisible by $m"
+fi
+
+echo ""
+
+# b) Supplied as command line arguments:
+echo "b) Supplied as command line arguments:"
+if [ $# -eq 0 ]
+then
+    echo "Enter valid cmd line arg"
+else
+    y=`expr $1 % $2`
+    if [ $y -eq 0 ]
+    then
+        echo "$1 is divisible by $2"
+    else
+        echo "$1 is not divisible by $2"
+    fi
 fi
