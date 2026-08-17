7baa3be5b3cbcc6e9cdaa9115111d2b4c6516d3a
Prajwal Jogi <164987205+prajwal918@users.noreply.github.com>
2026-08-17 14:14:53 +0530

    Update 1.cpp with handwritten code

--- 5 sem/PC++ Lab — Programming with C++ Lab/1.cpp
+++ 5 sem/PC++ Lab — Programming with C++ Lab/1.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 class BankAccount
 {
-char name[20];
-int accno;
-int balance;
-public:
-void read()
-{
-cout<<"Enter name"<<endl;
-cin>>name;
-cout<<"Enter Accno"<<endl;
-cin>>accno;
-cout<<"Enter Balance"<<endl;
-cin>>balance;
-}
-void print()
-{
-cout<<"Name: "<<name<<endl;
-cout<<"Account Number: "<<accno<<endl;
-cout<<"Balance: "<<balance<<endl;
-}
-void deposit()
-{
-int amount;
-cout<<"Enter amount"<<endl;
-cin>>amount;
-balance=balance+amount;
-}
-void withdraw()
-{
-int amount;
-cout<<"Enter amount to withdraw"<<endl;
-cin>>amount;
-if(amount>balance)
-{
-cout<<"Insufficient"<<endl;
-}
-else
-{
-balance=balance-amount;
-}
-}
-void put_balance()
-{
-cout<<"Account balance is: "<<balance<<endl;
-}
+    private:
+        int accno;
+        string name;
+        float balance;
+        int amt;
+    public:
+        void read()
+        {
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter account number: ";
+            cin >> accno;
+            cout << "Enter balance: ";
+            cin >> balance;
+        }
+        void put_balance()
+        {
+            cout << "Current balance: " << balance << endl;
+        }
+        void deposit()
+        {
+            cout << "\nEnter amount to be deposited: ";
+            cin >> amt;
+            balance = balance + amt;
+            put_balance();
+        }
+        void withdraw()
+        {
+            cout << "\nEnter amount to be withdrawn: ";
+            cin >> amt;
+            if (amt > balance)
+            {
+                cout << "Insufficient balance\n";
+            }
+            else
+            {
+                balance = balance - amt;
+                put_balance();
+            }
+        }
+        void display()
+        {
+            cout << "\nAccount Details:" << endl;
+            cout << "Name of the depositor: " << name << endl;
+            cout << "Account Number: " << accno << endl;
+            cout << "Balance: " << balance << endl;
+        }
 };
+
 int main()
 {
-BankAccount b1;
-b1.read();
-b1.print();
-b1.deposit();
-b1.withdraw();
-b1.put_balance();
-return 0;
+    BankAccount b;
+    b.read();
+    b.display();
+    b.deposit();
+    b.withdraw();
+    return 0;
 }
